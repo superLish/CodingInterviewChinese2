8c9b3b35a1b4629163d4ad517c3d1d7ca8989615
lisheng <s_lisheng@163.com>
2020-04-27 14:27:39 +0800

    study find in partially sort matrix done.

--- 04_FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.cpp
+++ 04_FindInPartiallySortedMatrix/FindInPartiallySortedMatrix.cpp
@@ -18,23 +18,21 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 整数，判断数组中是否含有该整数。
 
 #include <cstdio>
+#include<iostream>
+#include<cassert>
+using namespace std;
 
-bool Find(int* matrix, int rows, int columns, int number)
-{
+
+bool Find(int* matrix, int rows, int columns, int number) {
     bool found = false;
-
-    if(matrix != nullptr && rows > 0 && columns > 0)
-    {
+    if(matrix != nullptr && rows > 0 && columns > 0) {
         int row = 0;
         int column = columns - 1;
-        while(row < rows && column >=0)
-        {
-            if(matrix[row * columns + column] == number)
-            {
+        while(row < rows && column >=0) {
+            if(matrix[row * columns + column] == number) {
                 found = true;
                 break;
-            }
-            else if(matrix[row * columns + column] > number)
+            } else if(matrix[row * columns + column] > number)
                 -- column;
             else
                 ++ row;
@@ -45,8 +43,7 @@ bool Find(int* matrix, int rows, int columns, int number)
 }
 
 // ====================测试代码====================
-void Test(char* testName, int* matrix, int rows, int columns, int number, bool expected)
-{
+void Test(char* testName, int* matrix, int rows, int columns, int number, bool expected) {
     if(testName != nullptr)
         printf("%s begins: ", testName);
 
@@ -62,8 +59,7 @@ void Test(char* testName, int* matrix, int rows, int columns, int number, bool e
 //  4   7   10  13
 //  6   8   11  15
 // 要查找的数在数组中
-void Test1()
-{
+void Test1() {
     int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
     Test("Test1", (int*)matrix, 4, 4, 7, true);
 }
@@ -73,8 +69,7 @@ void Test1()
 //  4   7   10  13
 //  6   8   11  15
 // 要查找的数不在数组中
-void Test2()
-{
+void Test2() {
     int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
     Test("Test2", (int*)matrix, 4, 4, 5, false);
 }
@@ -84,8 +79,7 @@ void Test2()
 //  4   7   10  13
 //  6   8   11  15
 // 要查找的数是数组中最小的数字
-void Test3()
-{
+void Test3() {
     int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
     Test("Test3", (int*)matrix, 4, 4, 1, true);
 }
@@ -95,8 +89,7 @@ void Test3()
 //  4   7   10  13
 //  6   8   11  15
 // 要查找的数是数组中最大的数字
-void Test4()
-{
+void Test4() {
     int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
     Test("Test4", (int*)matrix, 4, 4, 15, true);
 }
@@ -106,8 +99,7 @@ void Test4()
 //  4   7   10  13
 //  6   8   11  15
 // 要查找的数比数组中最小的数字还小
-void Test5()
-{
+void Test5() {
     int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
     Test("Test5", (int*)matrix, 4, 4, 0, false);
 }
@@ -117,20 +109,17 @@ void Test5()
 //  4   7   10  13
 //  6   8   11  15
 // 要查找的数比数组中最大的数字还大
-void Test6()
-{
+void Test6() {
     int matrix[][4] = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
     Test("Test6", (int*)matrix, 4, 4, 16, false);
 }
 
 // 鲁棒性测试，输入空指针
-void Test7()
-{
+void Test7() {
     Test("Test7", nullptr, 0, 0, 16, false);
 }
 
-int main(int argc, char* argv[])
-{
+int main() {
     Test1();
     Test2();
     Test3();
@@ -141,4 +130,3 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-
