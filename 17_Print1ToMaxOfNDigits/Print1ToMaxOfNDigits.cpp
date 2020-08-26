b30341cff9a2f81bdc8baf44c43f8f3ef861db09
superLish <s_lisheng@163.com>
2020-08-26 15:41:03 +0800

    fix chinese error code.

--- 17_Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
+++ 17_Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
@@ -17,15 +17,14 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 打印出1、2、3一直到最大的3位数即999。
 
 #include <cstdio>
-#include <memory>
+#include <memory.h>
 
 void PrintNumber(char* number);
 bool Increment(char* number);
 void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index);
 
 // ====================方法一====================
-void Print1ToMaxOfNDigits_1(int n)
-{
+void Print1ToMaxOfNDigits_1(int n) {
     if (n <= 0)
         return;
 
@@ -33,41 +32,34 @@ void Print1ToMaxOfNDigits_1(int n)
     memset(number, '0', n);
     number[n] = '\0';
 
-    while (!Increment(number))
-    {
+    while (!Increment(number)) {
         PrintNumber(number);
     }
 
-    delete[]number;
+    delete [] number;
 }
 
 // 字符串number表示一个数字，在 number上增加1
 // 如果做加法溢出，则返回true；否则为false
-bool Increment(char* number)
-{
+bool Increment(char* number) {
     bool isOverflow = false;
     int nTakeOver = 0;
     int nLength = strlen(number);
 
-    for (int i = nLength - 1; i >= 0; i--)
-    {
+    for (int i = nLength - 1; i >= 0; i--) {
         int nSum = number[i] - '0' + nTakeOver;
         if (i == nLength - 1)
             nSum++;
 
-        if (nSum >= 10)
-        {
+        if (nSum >= 10) {
             if (i == 0)
                 isOverflow = true;
-            else
-            {
+            else {
                 nSum -= 10;
                 nTakeOver = 1;
                 number[i] = '0' + nSum;
             }
-        }
-        else
-        {
+        } else {
             number[i] = '0' + nSum;
             break;
         }
@@ -77,16 +69,14 @@ bool Increment(char* number)
 }
 
 // ====================方法二====================
-void Print1ToMaxOfNDigits_2(int n)
-{
+void Print1ToMaxOfNDigits_2(int n) {
     if (n <= 0)
         return;
 
     char* number = new char[n + 1];
     number[n] = '\0';
 
-    for (int i = 0; i < 10; ++i)
-    {
+    for (int i = 0; i < 10; ++i) {
         number[0] = i + '0';
         Print1ToMaxOfNDigitsRecursively(number, n, 0);
     }
@@ -94,16 +84,13 @@ void Print1ToMaxOfNDigits_2(int n)
     delete[] number;
 }
 
-void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index)
-{
-    if (index == length - 1)
-    {
+void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index) {
+    if (index == length - 1) {
         PrintNumber(number);
         return;
     }
 
-    for (int i = 0; i < 10; ++i)
-    {
+    for (int i = 0; i < 10; ++i) {
         number[index + 1] = i + '0';
         Print1ToMaxOfNDigitsRecursively(number, length, index + 1);
     }
@@ -112,18 +99,15 @@ void Print1ToMaxOfNDigitsRecursively(char* number, int length, int index)
 // ====================公共函数====================
 // 字符串number表示一个数字，数字有若干个0开头
 // 打印出这个数字，并忽略开头的0
-void PrintNumber(char* number)
-{
+void PrintNumber(char* number) {
     bool isBeginning0 = true;
     int nLength = strlen(number);
 
-    for (int i = 0; i < nLength; ++i)
-    {
+    for (int i = 0; i < nLength; ++i) {
         if (isBeginning0 && number[i] != '0')
             isBeginning0 = false;
 
-        if (!isBeginning0)
-        {
+        if (!isBeginning0) {
             printf("%c", number[i]);
         }
     }
@@ -132,8 +116,7 @@ void PrintNumber(char* number)
 }
 
 // ====================测试代码====================
-void Test(int n)
-{
+void Test(int n) {
     printf("Test for %d begins:\n", n);
 
     Print1ToMaxOfNDigits_1(n);
@@ -142,8 +125,7 @@ void Test(int n)
     printf("\nTest for %d ends.\n", n);
 }
 
-int main(int argc, char* argv[])
-{
+int main() {
     Test(1);
     Test(2);
     Test(3);
